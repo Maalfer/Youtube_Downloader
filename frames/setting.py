eb0164cfebb614efce3c86147fce6dfa1b1904fc
Desmon <61366533+desmonHak@users.noreply.github.com>
2023-01-17 00:06:26 +0100

    Add files via upload
    Signed-off-by: Desmon <61366533+desmonHak@users.noreply.github.com>

---
+++ frames/setting.py
@@ -0,0 +1,57 @@
+from sys import version, platform
+
+if version[0] == "3":
+    from tkinter import Toplevel, Menu,  Label
+elif version[0] == "2":
+    from Tkinter import (Toplevel, Menu,  Label)
+else:
+    print("Wtf que porongas paso aqui?!")
+
+
+from .error import UnknownOS
+
+from .load_config import calcular_file, load_file
+
+class Frame8:
+    
+    def __init__(self,
+                VentanaPadre,                                       # aqui se especifica la ventana que llamo a este frame
+                # parametro por defecto para la clase:
+                InstanciaPadre,
+                color_fondo=15,                                     # color de fondo por defecto de la venta
+                tamano_ventana=[400, 250],                          # tamaño de la ventana por defecto [x, y]
+                load_json=True                                      # cargar la configuracion del .json
+        ):
+        
+        if load_json:
+            ruta = calcular_file(__file__, "config-GUI")
+            print("[[ ",ruta)
+            config_data = load_file(ruta)
+            print(config_data)
+            
+            color_fondo = config_data["color-background"]
+            tamano_ventana = config_data["size"]
+            
+        
+        self.VentanaPadre = VentanaPadre
+        self.InstanciaPadre = InstanciaPadre
+        self.Frame = Toplevel()
+        self.color_fondo = color_fondo
+        self.Frame.resizable(config_data["resizable"][0], config_data["resizable"][1])  
+        
+        x = int((self.Frame.winfo_screenwidth()/2) - (tamano_ventana[0]/2)) # calculando la posicion de la ventana para que aparezca en la mitad de la pantalla
+        y = int((self.Frame.winfo_screenheight()/2) - (tamano_ventana[1]/2))
+        self.Frame.geometry('{}x{}+{}+{}'.format(tamano_ventana[0], tamano_ventana[1], x, y))  # Establecer un tamano a la ventana
+        
+        self.Frame.title("Ventana de ajustes. Idioma:{}".format(self.InstanciaPadre.idiomas.idioma)) # titulo de la ventana
+        self.Frame.config(bd=self.color_fondo) # Color de fondo
+        
+        self.menu_frame = Menu(self.Frame) # crear un menu donde poner pestanas
+        self.Frame.config(menu=self.menu_frame) # agregarle el menu
+        
+        self.menu_frame.add_command(label="Salir", command=self.killThisWindows)
+
+    
+    
+    def killThisWindows(self):
+        self.Frame.destroy()
